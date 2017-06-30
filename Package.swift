d5095883d11973d3af24c47751088d2346f03693
Kacy James <inquen@gmail.com>
2017-06-30 14:12:21 -0400

    Added Kanna

--- Package.swift
+++ Package.swift
@@ -3,8 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Billboard"
-
+    name: "Billboard",
     dependencies: [
         .Package(url: "https://github.com/tid-kijyun/Kanna.git", majorVersion: 2)
     ]
