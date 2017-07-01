18993f29a30d99653051bfa582c0e3869dece3c3
Kacy James <inquen@gmail.com>
2017-07-01 11:15:08 -0400

    Added empty init method

--- Sources/Billboard.swift
+++ Sources/Billboard.swift
@@ -3,6 +3,7 @@ import Kanna
 
 public class Billboard {
   var htmlContents: String?
+  public init() {}
   public init(uniformResourceLocator: String) {
     let url = URL(string: uniformResourceLocator)
     if let doc = HTML(url: url!, encoding: .utf8) {
