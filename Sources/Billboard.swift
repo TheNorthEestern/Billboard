0a079b391ddf25313f502c52cd1d137f4b70be4b
Kacy James <inquen@gmail.com>
2017-07-02 18:58:06 -0400

    Testing Kanna parsing

--- Sources/Billboard.swift
+++ Sources/Billboard.swift
@@ -2,12 +2,28 @@ import Foundation
 import Kanna
 
 public class Billboard {
-  var htmlContents: String?
-  public init() {}
+  private var hot100Rows : XPathObject?
+  public var htmlContents: String?
+  public var returnedDocument: HTMLDocument?
   public init(uniformResourceLocator: String) {
     let url = URL(string: uniformResourceLocator)
     if let doc = HTML(url: url!, encoding: .utf8) {
-      print(doc)
+      // returnedDocument = doc
+      // htmlContents = doc.title
+    }
+  }
+  
+  public func listHot100() {
+    if let doc = HTML(url: URL(string: "http://www.billboard.com/charts/hot-100")!, encoding: .utf8) {
+      hot100Rows = doc.css(".chart-row")
+      if let rows = hot100Rows {
+        for row in rows {
+          let songTitle = row.at_css(".chart-row__song")!.content!
+          let songArtist = row.at_css(".chart-row__artist")!.content!
+          let chartRank = row.at_css(".chart-row__current-week")!.content!
+          print("\(chartRank) --- \(songTitle) | \(songArtist.trimmingCharacters(in: .whitespacesAndNewlines))")
+        }
+      }
     }
   }
 }
