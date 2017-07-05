8b04f05e3b4a2cbc46bb3ea8199ab4f64ed35e7c
Kacy James <inquen@gmail.com>
2017-07-04 23:53:10 -0400

    Added model file

--- Sources/Billboard.swift
+++ Sources/Billboard.swift
@@ -3,27 +3,37 @@ import Kanna
 
 public class Billboard {
   private var hot100Rows : XPathObject?
+  private var hot100 : [BillboardEntry]?
   public var htmlContents: String?
   public var returnedDocument: HTMLDocument?
   public init(uniformResourceLocator: String) {
-    let url = URL(string: uniformResourceLocator)
+    /* let url = URL(string: uniformResourceLocator)
     if let doc = HTML(url: url!, encoding: .utf8) {
       // returnedDocument = doc
       // htmlContents = doc.title
+    } */
+  }
+  
+  public func fetchHot100() {
+    if let doc = HTML(url: URL(string: "http://www.billboard.com/charts/hot-100")!, encoding: .utf8) {
+      if let rows = doc.css(".chart-row") as XPathObject? {
+        for row in rows {
+          let songTitle = row.at_css(".chart-row__song")!.content!
+          let artistName = row.at_css(".chart-row__artist")!.content!
+          let currentRank = row.at_css(".chart-row__current-week")!.content!
+          let previousRank = row.at_css(".chart-row__last-week")!.content!
+          
+          hot100?.append(BillboardEntry(songTitle: songTitle, artistName: artistName.trimmingCharacters(in: .whitespacesAndNewlines), currentRank: currentRank, previousRank: previousRank.characters.last!))
+        }
+      }
     }
   }
   
   public func listHot100() {
-    if let doc = HTML(url: URL(string: "http://www.billboard.com/charts/hot-100")!, encoding: .utf8) {
-      hot100Rows = doc.css(".chart-row")
-      if let rows = hot100Rows {
+      if let rows = hot100 {
         for row in rows {
-          let songTitle = row.at_css(".chart-row__song")!.content!
-          let songArtist = row.at_css(".chart-row__artist")!.content!
-          let chartRank = row.at_css(".chart-row__current-week")!.content!
-          print("\(chartRank) --- \(songTitle) | \(songArtist.trimmingCharacters(in: .whitespacesAndNewlines))")
+          print("\(row.currentRank) --- \(row.songTitle) | \(row.artistName))")
         }
-      }
     }
   }
 }
---
+++ Sources/BillboardEntry.swift
@@ -0,0 +1,26 @@
+//
+//  BillboardEntry.swift
+//  Billboard
+//
+//  Created by Kacy James on 7/4/17.
+//
+//
+
+import Foundation
+
+struct BillboardEntry {
+  public let songTitle : String
+  public let artistName : String
+  public let currentRank : String
+  public let previousRank : Character
+  public var entryContent : String {
+    return "\(currentRank) -- \(songTitle) -- \(artistName)"
+  }
+  
+  init(songTitle: String, artistName: String, currentRank: String, previousRank: Character) {
+    self.songTitle = songTitle
+    self.artistName = artistName
+    self.currentRank = currentRank
+    self.previousRank = previousRank
+  }
+}
